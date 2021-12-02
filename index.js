e1873ec2600ad67a67ea28a5133d55d64cdc3f8c
yaser <yaser@superblacksoftware.net>
2021-12-02 21:32:05 +0000

    totum-rename

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 // import {renderer, camera, app} from 'app';
 import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
-import metaversefile from 'metaversefile';
-const {useFrame} = metaversefile;
+import totum from 'totum';
+const {useFrame} = totum;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
